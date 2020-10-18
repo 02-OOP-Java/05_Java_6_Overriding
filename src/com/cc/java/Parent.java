f7787ef7b5463d8e187c417a3812b8ad3a47b9cb
Peter Schmidt <schmidt.multimedia@gmail.com>
2020-10-19 01:20:22 +0200

    Add files via upload

---
+++ src/com/cc/java/Parent.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Parent {
+
+	public void sayHello() {
+		System.out.println("Hello from parent!");
+	}
+	
+	
+	public void sayHello(String name) {
+		System.out.println("Parent says, Hi " + name + "!");
+	}
+	
+}
